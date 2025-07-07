8244aadd4b6746a717a14867d4599a8d69569156
Rehu <mgt.kk16@gmail.com>
2025-07-07 21:57:22 +0530

    Update main.dart

--- android/ios/lib/main.dart
+++ android/ios/lib/main.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:made_for_iram/screens/splash.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  
+  const InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+  
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Made for Iram',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFFE91E63),
+          primary: const Color(0xFFE91E63),
+          secondary: const Color(0xFFFF80AB),
+        ),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
